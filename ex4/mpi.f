4e7000f17e8d93457c09383e528575599e435414
Didrik Jonassen <jonassen.didrik@gmail.com>
2014-02-11 19:45:52 +0100

    Gotta do some debugging.

--- ex4/mpi.f
+++ ex4/mpi.f
@@ -13,7 +13,7 @@ PROGRAM EX4_MPI
     IF (RANK .EQ. 0) THEN
         WRITE (*, "('S = PI^2/6 = 'F7.5)") S
     END IF  
-    DO K = 14,14
+    DO K = 4,14
         SK = S_N(2**K, RANK, SIZE)
         IF (RANK .EQ. 0) THEN
             WRITE (*, *)
@@ -25,33 +25,35 @@ PROGRAM EX4_MPI
     CALL MPI_FINALIZE(IERROR)
 END PROGRAM
 
-REAL FUNCTION S_N(N, RANK, SIZE)
+REAL FUNCTION S_N(N, RANK, SIZ)
     USE MPI
-    INTEGER :: N, I, J, RANK, SIZE, IERROR
+    INTEGER :: N, I, J, RANK, SIZ, IERROR
     REAL :: SUM
-    REAL, DIMENSION(N/SIZE) :: V
+    REAL, DIMENSION(N/SIZ) :: V
+      PRINT *, SIZE(V)
+      PRINT *, N/SIZ
 
     IF(RANK .EQ. 0) THEN
-        DO I = SIZE-1, 1, -1
-            DO J = 1, N/SIZE
-                V(J) = 1.0/(J+(I*N)/SIZE)**2
+        DO I = SIZ-1, 1, -1
+            DO J = 1, N/SIZ
+                V(J) = 1.0/(J+(I*N)/SIZ)**2
             END DO
-            CALL MPI_SEND(V, N/SIZE, MPI_REAL, I, 0, MPI_COMM_WORLD, IERROR)
+            CALL MPI_SEND(V, N/SIZ, MPI_REAL, I, 0, MPI_COMM_WORLD, IERROR)
         END DO
-        DO J = 1, N/SIZE
+        DO J = 1, N/SIZ
             V(J) = 1.0/J**2
         END DO
         SUM = 0.0
-        DO J = N/SIZE, 1, -1
+        DO J = N/SIZ, 1, -1
             SUM = SUM + V(J)
         END DO
     ELSE
-        CALL MPI_RECV(V, N/SIZE, MPI_REAL, 0, 0, MPI_COMM_WORLD, IERROR)
+        CALL MPI_RECV(V, N/SIZ, MPI_REAL, 0, 0, MPI_COMM_WORLD, IERROR)
         SUM = 0.0
-        DO I = N/SIZE, 1, -1
+        DO I = N/SIZ, 1, -1
             SUM = SUM + V(I)
         END DO
     END IF
-    CALL MPI_REDUCE(SUM, S_N, 1, MPI_REAL, MPI_SUM, 0, MPI_COMM_WORLD, IERROR)
+    CALL MPI_REDUCE(SUM, S_N, N/SIZ, MPI_REAL, MPI_SUM, 0, MPI_COMM_WORLD, IERROR)
     RETURN
 END FUNCTION
